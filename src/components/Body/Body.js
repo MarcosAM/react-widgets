dee89919f68e4eb327e57d4a58d9121c274ac801
Marcos Mourão <marcosarruda.m@gmail.com>
2019-06-18 14:13:41 -0300

    Body gera widgets baseado no state widgets

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -7,18 +7,27 @@ import Fab from '../Fab'
 import AddIcon from '@material-ui/icons/Add'
 import EditChartDialog from '../EditChartDialog'
 
+//TODO adicionar a possibilidade de alterar nome de widget
 class Body extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            widgets: [
+                [{
+                    name: 'Profits',
+                    data: [1, 5, 3]
+                }, {
+                    name: 'Other Profits',
+                    data: [2, 3, 8]
+                }]
+            ],
             series: [{
                 name: 'Profits',
                 data: [1, 5, 3]
             }, {
                 name: 'Other Profits',
                 data: [2, 3, 8]
-            }
-            ],
+            }],
             isShowingDialog: false
         }
 
@@ -26,6 +35,7 @@ class Body extends Component {
         this.updateWidget = this.updateWidget.bind(this)
     }
 
+    //TODO passar para o EditDialog informação de qual widget está sendo alterado
     menuItens() {
         return (
             [{ text: 'Edit', click: () => this.setDialogOpen(true) }]
@@ -41,6 +51,22 @@ class Body extends Component {
         this.setState(state => ({ series: newSeries, isShowingDialog: false }))
     }
 
+    renderWidgets() {
+        const widgets = [...this.state.widgets.entries()]
+
+        return (widgets.map(entry => {
+            const [key, value] = entry
+
+            return (
+                <Widget
+                    title='Chart'
+                    menuItens={this.menuItens()}>
+                    <LineChart series={value} />
+                </Widget>
+            )
+        }))
+    }
+
     render() {
         return (
             <div className={this.props.classes.body} >
@@ -48,6 +74,7 @@ class Body extends Component {
                     menuItens={this.menuItens()} >
                     <LineChart series={this.state.series} />
                 </Widget >
+                {this.renderWidgets()}
                 <EditChartDialog
                     series={this.state.series}
                     isShowing={this.state.isShowingDialog}
--- src/components/Body/styles.js
+++ src/components/Body/styles.js
@@ -1,6 +1,7 @@
 export default theme => ({
     body: {
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         padding: theme.spacing(3),
