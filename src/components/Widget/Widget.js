254448c957a4b461ff2f519a5cd4b037ff9c92a9
Marcos Mourão <marcosarruda.m@gmail.com>
2019-06-18 13:57:47 -0300

    Refactor: Alterações visuais no componente Widget

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core';
 import styles from './styles';
 import KebabMenu from '../KebabMenu';
 import Divider from '@material-ui/core/Divider';
+import CardHeader from '@material-ui/core/CardHeader';
 
 const Widget = ({ title, classes, children, menuItens }) => (
   <Card className={classes.card}>
+    <CardHeader title={title}
+      action={<KebabMenu menuItens={menuItens} />} />
+    <Divider variant="fullWidth" />
     <CardContent>
-      <div className={classes.header}>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          {title}
-        </Typography>
-        <KebabMenu menuItens={menuItens} />
-      </div>
-      <Divider variant="fullWidth" />
       <div className={classes.body}>
         {children}
       </div>
     </CardContent>
-  </Card>
+  </Card >
 )
 
 export default withStyles(styles)(Widget)
--- src/components/Widget/styles.js
+++ src/components/Widget/styles.js
@@ -2,14 +2,6 @@ const styles = theme => ({
     card: {
         minWidth: '80%',
     },
-    header: {
-        display: 'flex',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: theme.spacing(1)
-    },
     body: {
         padding: theme.spacing(1)
     }
